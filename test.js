cac2f99bc4c1dcb11e2c48f93f6991aa8ca7b693
walterEcwid <56197357+walterEcwid@users.noreply.github.com>
2020-01-24 19:33:03 +0400

    Update test.js

--- test.js
+++ test.js
@@ -1,1 +1,4 @@
-TEXT_TEXT
+window.ec = window.ec || Object();
+window.ec.storefront = window.ec.storefront || Object();
+window.ec.storefront.show_signin_link = false; // hides the sign in link
+Ecwid.refreshConfig();
