fae02b72c9010768807914cea5c1fad8785a22ed
walterEcwid <56197357+walterEcwid@users.noreply.github.com>
2020-01-27 18:51:17 +0400

    Update test.js

--- test.js
+++ test.js
@@ -1,9 +1,4 @@
-window.ec = window.ec || Object();
-window.ec.config = window.ec.config || Object();
-window.ec.config.chameleon = window.ec.config.chameleon || Object();
-window.ec.config.chameleon.colors = {
-  'color-button':'#BBE3EB',
-  'color-price':'#EC6B00'
-}
-
-Ecwid.refreshConfig();
+Ecwid.OnPageLoaded(function () {
+var storeId = Ecwid.getOwnerId()
+console.log(storeId);
+});
