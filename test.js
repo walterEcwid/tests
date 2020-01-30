e816e06c906f14903e05bf94c90af95c7e97de38
walterEcwid <56197357+walterEcwid@users.noreply.github.com>
2020-01-30 22:11:36 +0400

    Update test.js

--- test.js
+++ test.js
@@ -1,5 +1,13 @@
-console.info("ЗАГРУЗИСЬ!");
-
 Ecwid.OnPageLoaded.add(function(page) {
-  console.log("ПАЖАЛУСТА ЗАРАБОТАЙ!");
+  window.ec = window.ec || Object();
+  window.ec.config = window.ec.config || Object();
+  window.ec.config.chameleon = window.ec.config.chameleon || Object();
+  window.ec.config.chameleon.colors = {
+    'color-background':'#D3D3D3',
+    'color-foreground':'#4EA3F0',
+    'color-link':'#FF0606',
+    'color-button':'#4EA3F0',
+    'color-price':'#FF0606'
+  }
+Ecwid.refreshConfig();
 });
