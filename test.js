5a6c45db61f5209426e7e4af7692d091131acd0b
walterEcwid <56197357+walterEcwid@users.noreply.github.com>
2020-01-31 23:12:37 +0400

    Update test.js

--- test.js
+++ test.js
@@ -1,14 +1,6 @@
-console.log("Кастомное приложение загружено!"); /*
-  window.ec = window.ec || Object();
-  window.ec.config = window.ec.config || Object();
-  window.ec.config.chameleon = window.ec.config.chameleon || Object();
-  window.ec.config.chameleon.colors = {
-    'color-background':'#D3D3D3',
-    'color-foreground':'#4EA3F0',
-    'color-link':'#FF0606',
-    'color-button':'#4EA3F0',
-    'color-price':'#FF0606'
-  }
-  Ecwid.refreshConfig(); */
-//Ecwid.OnPageLoaded.add(function(page) {
-//});
+console.log("Кастомное приложение загружено!");
+
+Ecwid.OnPageLoaded.add(function(page) {
+  var lang = Ecwid.getStorefrontLang();
+  console.log(lang);
+});
